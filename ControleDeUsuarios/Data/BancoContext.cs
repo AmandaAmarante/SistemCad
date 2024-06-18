e64ca39ea999feb5f58b4ae70eaba78af9f568fe
Amanda Amarante dos Santos <amanda.amarante@altasolucoes.com.br>
2024-06-18 20:42:44 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeUsuarios/Data/BancoContext.cs
@@ -0,0 +1,19 @@
+﻿using ControleDeUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeUsuarios.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) :
+            base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
